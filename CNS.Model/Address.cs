99586b84d648f26b8a3cc7002d3ed49491c2125b
Pal <rpalaniappan@gmail.com>
2015-04-08 16:34:49 -0400

    SqlLite through Entity Framework

---
+++ CNS.Model/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNS.Model
+{
+    public class Address
+    {
+        public string Address1;
+        public string Address2;
+        public string City;
+        public string Province;
+        public string PostalCode;
+    }
+}
---
+++ ConsoleFrontEnd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CNS.BusinessLayer;
+using CNS.Model;
+
+namespace ConsoleFrontEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing Addresses");
+            AddressController l_addressController = new AddressController();
+            foreach (Address l_address in l_addressController.GetAllAddresses())
+            {
+                Console.WriteLine(l_address.Address1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
